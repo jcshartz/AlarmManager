44414c7718b725df95cdcf8cc09697d2b0d9d9db
Jay Shartzer <3780990+jcshartz@users.noreply.github.com>
2018-12-08 23:48:46 -0600

    Initial Commit
    A first stab at making a basic alarm app. More details can be found in
    the README.md file and the TODO file at the upper level of the project
    folder structure.

---
+++ app/src/main/java/com/conceptexperiment/alarmmanager/AddAlarmActivity.java
@@ -0,0 +1,9 @@
+package com.conceptexperiment.alarmmanager;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Screen where the user can create the alarm
+ */
+public class AddAlarmActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/conceptexperiment/alarmmanager/MainActivity.java
@@ -0,0 +1,23 @@
+package com.conceptexperiment.alarmmanager;
+
+import android.app.AlarmManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+/* Main To-Dos
+TODO: get started
+
+ */
+
+/**
+ * Entry point for the application.
+ * The user can view alarms if they have already been created. The user can choose to edit those
+ * alarms, delete those alarms, or create new ones. The user can also choose to enter the Settings
+ * activity where various additional options exist.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    Button addAlarm;
+
+
+}
